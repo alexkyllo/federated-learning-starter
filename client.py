1ea5da256d5f8a7b43c84c2e4af378403115ad22
Alex Kyllo <alex.kyllo@gmail.com>
2022-10-28 14:45:28 -0700

    save work

--- client.py
+++ client.py
@@ -1,9 +1,26 @@
+import argparse
 from flwr.client import NumPyClient
 from flwr.common.typing import Config, Dict, NDArrays, Scalar, Tuple
+from loguru import logger
+
+from model import MyClassifier
 
 
 class MyClient(NumPyClient):
     """A simple flower client for demonstration purposes."""
+
+    def __init__(self, model, cid: int) -> None:
+        self.model = model
+        self.cid = cid
+
+     def set_parameters(self, parameters: NDArrays) -> None:
+        """Set the PyTorch module parameters from a list of NumPy arrays.
+        Parameters
+        ----------
+        parameters: List[numpy.ndarray]
+            The desired local model parameters as a list of NumPy ndarrays.
+        """
+        # TODO
 
     def get_parameters(self, config: Dict[str, Scalar]) -> NDArrays:
         """Return the current local model parameters.
@@ -37,9 +54,40 @@ class MyClient(NumPyClient):
         return
 
 
-def main():
-    client = MyClient()
+def load_data(client_id: int):
+    """Get a subset of the training data for one client."""
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+    return ((train_x, train_y), (test_x, test_y))
 
+
+def start_client(cid: int, batch_size: int):
+    """Start a client for training."""
+    model = MyClassifier(batch_size)
+    train_data, test_data = load_data(cid)
+    client = MyClient(model, cid)
+    logger.info("Starting client # {}", cid)
+
+def get_args():
+    """Get command-line arguments."""
+    parser = argparse.ArgumentParser(description="Flower Client for demo.")
+    parser.add_argument(
+        "cid",
+        type=int,
+        help="The current client ID. Used for splitting the dataset.",
+    )
+    parser.add_argument(
+        "--num-clients",
+        default=2,
+        type=int,
+        help="Total number of clients for federated training.",
+    )
+    parser.add_argument("--batch-size", default=32, type=int, help="Batch size")
 
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    cid = int(args.cid)
+    batch_size = int(args.batch_size)
+    start_client(cid, batch_size)
--- model.py
+++ model.py
@@ -1,1 +1,24 @@
 """The example ML model to train in a federated setting."""
+from sklearn.neural_network import MLPClassifier
+
+
+class MyClassifier:
+    def __init__(self, batch_size):
+        """init."""
+        self.module = MLPClassifier(
+            solver="adam",
+            alpha=1e-5,
+            hidden_layer_sizes=(5, 2),
+            batch_size=batch_size,
+            verbose=True,
+        )
+
+    def fit(self, X, y, epochs):
+        """Fit the model on a dataset."""
+        for _ in range(epochs):
+            self.module.partial_fit(X, y)
+        return self.module
+
+    def predict_proba(self, X):
+        """Predict for a dataset."""
+        return self.module.predict_proba(X)
